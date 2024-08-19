4c5284d429867b13d4d99d2ed680441f39b6f390
Azizbek Abdurahmonov <abdurahmonov-azizbek@outlook.com>
2024-08-19 03:02:00 -0700

    Added user entity

---
+++ src/Exams.Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using Exams.Domain.Entities.Common;
+using Exams.Domain.Enums;
+using System.Globalization;
+
+namespace Exams.Domain.Entities
+{
+    public class User : EntityBase
+    {
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public string PhoneNumber { get; set; } = default!;
+        public Role Role { get; set; } = Role.User;
+    }
+}
---
+++ src/Exams.Domain/Enums/Role.cs
@@ -0,0 +1,9 @@
+﻿namespace Exams.Domain.Enums
+{
+    public enum Role
+    {
+        User = 0,
+        Admin = 1,
+        System = 2
+    }
+}
